d3f37b92ec590e2426b4ec12ec075856bca04520
liyankova <liyannkova@gmail.com>
2025-09-19 13:12:34 +0700

    trying readjust dotfiles.nix before rebooting

--- home/modules/dotfiles.nix
+++ home/modules/dotfiles.nix
@@ -2,23 +2,12 @@
 
 let
   rawDotfilesPath = inputs.dotfiles-raw;
-
   topLevelItems = builtins.attrNames (builtins.readDir rawDotfilesPath);
 
-  # List of all packages to be managed via xdg.configFile
-  xdgPackageFolders = [
-    "btop"
-    "fastfetch"
-    "hypr"
-    "kitty"
-    "oh-my-posh"
-    "rofi"
-    "swaync"
-    "swappy"
-    "waybar"
-    "wlogout"
-    "yazi"
-    "wallust"
+  # --- XDG Configs (~/.config) ---
+  xdgFolders = [
+    "btop" "fastfetch" "hypr" "kitty" "oh-my-posh" "rofi"
+    "swaync" "swappy" "waybar" "wlogout" "yazi" "wallust"
   ];
 
   xdgConfigs = pkgs.lib.listToAttrs (map (folderName:
@@ -28,34 +17,33 @@ let
     in
     {
       name = folderName;
-      value = {
-        source =
-          if builtins.pathExists nestedConfigPath
-          then nestedConfigPath
-          else packagePath;
-        recursive = true;
-      };
+      value.source =
+        if builtins.pathExists nestedConfigPath
+        then nestedConfigPath
+        else packagePath;
     }
-  ) xdgPackageFolders);
+  ) xdgFolders);
 
+
+  # --- Non-XDG Configs ($HOME) ---
+  # homeFileConfigs = {
+  #   ".zshrc" = {
+  #     source = "${rawDotfilesPath}/zsh/.zshrc";
+  #     enable = builtins.pathExists "${rawDotfilesPath}/zsh/.zshrc";
+  #   };
+  #   ".tmux.conf" = {
+  #     source = "${rawDotfilesPath}/tmux/.tmux.conf";
+  #     enable = builtins.pathExists "${rawDotfilesPath}/tmux/.tmux.conf";
+  #   };
+  #   ".config/oh-my-tmux" = {
+  #     source = "${rawDotfilesPath}/oh-my-tmux/.config/oh-my-tmux";
+  #     enable = builtins.pathExists "${rawDotfilesPath}/oh-my-tmux/.config/oh-my-tmux";
+  #   };
+  # };
+  #
 in
 {
   xdg.configFile = xdgConfigs;
 
-  home.file = {
-    ".zshrc" = {
-      source = "${rawDotfilesPath}/zsh/.zshrc";
-      enable = builtins.pathExists "${rawDotfilesPath}/zsh/.zshrc";
-    };
-
-    ".tmux.conf" = {
-      source = "${rawDotfilesPath}/tmux/.tmux.conf";
-      enable = builtins.pathExists "${rawDotfilesPath}/tmux/.tmux.conf";
-    };
-
-    ".config/oh-my-tmux" = {
-      source = "${rawDotfilesPath}/oh-my-tmux/.config/oh-my-tmux";
-      enable = builtins.pathExists "${rawDotfilesPath}/oh-my-tmux/.config/oh-my-tmux";
-    };
-  };
+  home.file = homeFileConfigs;
 }
